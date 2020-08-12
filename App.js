6f5069172e4a5838069b52928e1f0bebe5b217ff
darrylgit <darrylsmithup@gmail.com>
2020-08-12 10:03:35 -0400

    Abstract context logic out into its own function

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-import { BlogProvider } from "./src/context/BlogContext";
+import { Provider } from "./src/context/BlogContext";
 
 import IndexScreen from "./src/screens/IndexScreen";
 
@@ -22,8 +22,8 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };

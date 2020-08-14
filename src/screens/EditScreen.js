8c5ae46a61d5d4cadd42083e36a5a619e999f638
darrylgit <darrylsmithup@gmail.com>
2020-08-14 11:27:18 -0400

    Eviscerate Create and Edit screens and reap their souls into BlogPostForm

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,6 +1,8 @@
-import React, { useState, useContext } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
@@ -8,15 +10,7 @@ const EditScreen = ({ navigation }) => {
     blogPost => blogPost.id === navigation.getParam("id")
   );
 
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
-
-  return (
-    <View>
-      <Text>Edit title:</Text>
-      <TextInput value={title} onChangeText={newTitle => setTitle(newTitle)} />
-    </View>
-  );
+  return <BlogPostForm />;
 };
 
 const styles = StyleSheet.create({});

61ed57fbce4fcbf6b837ef2b841f0763cf8d110e
darrylgit <darrylsmithup@gmail.com>
2020-08-14 10:37:47 -0400

    Navigate users back after blog post creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -20,8 +20,9 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = dispatch => {
-  return (title, content) => {
+  return (title, content, callback) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
+    callback();
   };
 };
 
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -24,7 +24,11 @@ const CreateScreen = ({ navigation }) => {
       <View style={styles.button}>
         <Button
           title='Add Blog Post'
-          onPress={() => addBlogPost(title, content)}
+          onPress={() =>
+            addBlogPost(title, content, () => {
+              navigation.navigate("Index");
+            })
+          }
         />
       </View>
     </View>

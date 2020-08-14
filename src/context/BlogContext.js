4746546f191cce4ac06d535b274d267a7640ea05
darrylgit <darrylsmithup@gmail.com>
2020-08-14 14:31:51 -0400

    Delete blog posts with API

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,8 @@
-import _ from "lodash";
 import createDataContext from "./createDataContext";
 import jsonServer from "../api/jsonServer";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: _.uniqueId("blog_id_"),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
     case "get_blogposts":
       return action.payload;
     case "edit_blogpost":
@@ -29,7 +19,7 @@ const blogReducer = (state, action) => {
 const addBlogPost = dispatch => {
   return async (title, content, callback) => {
     await jsonServer.post("/blogposts", { title, content });
-    // dispatch({ type: "add_blogpost", payload: { title, content } });
+
     callback && callback();
   };
 };
@@ -50,7 +40,8 @@ const editBlogPost = dispatch => {
 };
 
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
@@ -58,12 +49,5 @@ const deleteBlogPost = dispatch => {
 export const { Context, Provider } = createDataContext(
   blogReducer,
   { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
-  [
-    {
-      title: "Test Post",
-      content:
-        "Cornhole chambray truffaut craft beer bushwick food truck ugh asymmetrical cold-pressed. Green juice ennui edison bulb distillery sustainable vape, viral lomo letterpress bicycle rights photo booth wayfarers leggings kombucha master cleanse. Flannel air plant distillery hoodie umami. Mlkshk fam la croix, four dollar toast swag tumeric leggings next level messenger bag. Sriracha actually iceland small batch live-edge succulents cornhole ethical. Tumeric VHS affogato listicle, glossier you probably haven't heard of them live-edge selvage. Yr truffaut beard, tumeric pok pok vinyl art party synth. \n\nTaiyaki leggings mixtape skateboard cardigan pop-up +1. Helvetica skateboard cornhole cray, kitsch 3 wolf moon single-origin coffee bushwick pinterest. Craft beer farm-to-table glossier fixie, palo santo thundercats irony venmo. Roof party VHS shaman readymade snackwave. Kickstarter meditation live-edge mlkshk messenger bag. Meggings shabby chic vice, dreamcatcher drinking vinegar palo santo pug chicharrones literally bicycle rights vexillologist ennui butcher before they sold out four loko.",
-      id: "1"
-    }
-  ]
+  []
 );
